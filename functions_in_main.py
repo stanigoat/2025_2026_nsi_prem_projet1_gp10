aec75dc8e5769cedf341889ae2761620c381559f
stanigoat <stan.paturel@gmail.com>
2025-10-09 11:49:07 +0200

    Avancement de la fonction connection et création des outils nécessaires

--- functions_in_main.py
+++ functions_in_main.py
@@ -1,8 +1,28 @@
 #Vous trouverez ici le code des différentes fonctions uniques à la fonction main
+quitting_words = ["quit", "stop", "bye", ""]
+
+def load_database():
+    with open("database.json", "r") as f:
+        users_database = json.load(f)
+    return users_database
 
 def ask_user_choice():
-    return choice = input("Veuillez entrez votre choix : ")
+    choice = input("Veuillez entrez votre choix : ")
+    return choice
 
 def re_ask_user_choice():
-    return choice = input("Veuillez choisir une option dans la liste ci-dessus : ")
+    choice = input("Veuillez choisir une option dans la liste ci-dessus : ")
+    return choice
+
+def register_user():
+    users_database = load_database()
+    user_answ = input("Veuillez rentrer votre prénom")
+    while user_answ not in quitting_words:
+        for answ_check in users_database:
+            if user_answ == answ_check:
+                pin_check = int(input("Veuillez rentrer votre pin"))
+                if pin_check == users_database[answ_check]["PIN"]:
+                    print("Connexion réussie")
+                else: print("Veuillez réessayer")
+            else: print("Votre nom n'est pas dans notre base de données")
 
--- register_user.py
+++
@@ -1,4 +0,0 @@
-#Vous trouverez ici toutes les fonctions liées à l'identification de l'utilisateur
-
-def register_user():
-    user_answ = input("Veuillez rentrer")
